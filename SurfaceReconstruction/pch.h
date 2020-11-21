cd465a17cb8b84d0274f3a5e30abb154d1ee9999
Adrien Hadj-Chaib <adriennice06@yahoo.fr>
2020-11-21 00:48:21 -0800

    Initial Commit

---
+++ SurfaceReconstruction/pch.h
@@ -0,0 +1,35 @@
+#ifndef PRECOMPILED_HEADER
+#define PRECOMPILED_HEADER
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/string_cast.hpp> 
+
+#include <cmath>
+#include <vector>
+#include <list>
+#include <string>
+#include <memory>
+#include <stack>
+#include <algorithm>
+
+#include <stdlib.h>
+#include <cstdlib>
+#include <cstddef>
+#include <unistd.h>
+#include <dlfcn.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#endif //PRECOMPILED_HEADER
